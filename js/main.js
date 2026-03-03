c0ce0a18e64039f6cef7f1edd806812ab753811f
PraptiH <praptiantara1892@gmail.com>
2026-03-03 15:57:47 +0600

    Display the hidden panel

--- js/main.js
+++ js/main.js
@@ -23,15 +23,36 @@ function loadMilestones() {
             return ` <div class="flex items-center gap-3 mx-3 px-[8%] py-[2%] border-b-1 border-gray-400">
                     <div class="w-3 h-3 rounded-sm bg-white"></div>
                     <p class="text-white">${milestone.name} 1</p>
-                    <button><i class="fa-solid fa-angle-down" style="color: rgb(255, 255, 255);"></i></button>
+                    <button onclick="displayModule(this)"><i class="fa-solid fa-angle-down" style="color: rgb(255, 255, 255);"></i></button>
                 </div>
 
-                <div class="hidden_panel">
-                    <div class="module border-b-1">
-                        <p>Module Name</p>
-                    </div>
+                <div class="hidden hiddenPanel mt-2 border-2 border-white ml-25 mr-5">
+                ${milestone.modules
+                    .map(function (module) {
+                        return `<div class="module mt-2">
+                        <p class="text-white">${module.name}</p>
+                    </div>`
+                    }).join("")
+                }
                 </div>`
         }).join("")
     })
 }
+
+function displayModule(module) {
+    const currentPanel = module.parentNode.nextElementSibling
+
+    if (currentPanel.classList.contains("hidden")) {
+
+        document.querySelectorAll('.hiddenPanel').forEach(panel => {
+            panel.classList.add('hidden');
+        });
+        currentPanel.classList.remove("hidden")
+    }
+    else {
+        currentPanel.classList.add("hidden")
+    }
+}
+
+
 loadMilestones()
