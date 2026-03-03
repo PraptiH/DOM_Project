024306f40dacda7ca8dd4cb5d23b705cef743292
PraptiH <praptiantara1892@gmail.com>
2026-03-03 16:50:55 +0600

    Toogle the visible panel design when clicked

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function loadMilestones() {
 
     document.querySelectorAll('.milestones').forEach(milestones => {
         milestones.innerHTML = milestonesData.map(function (milestone) {
-            return ` <div class="flex items-center gap-3 mx-3 px-[8%] py-[2%] border-b-1 border-gray-400">
+            return ` <div class="visiblePanel flex items-center gap-3 mx-3 px-[8%] py-[2%] border-b-1 border-gray-400">
                     <div class="w-3 h-3 rounded-sm bg-white"></div>
                     <p class="text-white">${milestone.name} 1</p>
                     <button onclick="displayModule(this)"><i class="fa-solid fa-angle-down" style="color: rgb(255, 255, 255);"></i></button>
@@ -41,16 +41,25 @@ function loadMilestones() {
 
 function displayModule(module) {
     const currentPanel = module.parentNode.nextElementSibling
+    const currentMilestone = module.parentNode
 
     if (currentPanel.classList.contains("hidden")) {
 
         document.querySelectorAll('.hiddenPanel').forEach(panel => {
             panel.classList.add('hidden');
         });
+
+        document.querySelectorAll('.visiblePanel').forEach(milestone => {
+            milestone.classList.remove('font-bold');
+        });
+
         currentPanel.classList.remove("hidden")
+        currentMilestone.classList.add('font-bold')
     }
+    // if the currently clicked milestone is currently open it will close it and vice-versa.
     else {
-        currentPanel.classList.add("hidden")
+        currentPanel.classList.toggle("hidden")
+        currentMilestone.classList.toggle("font-bold")
     }
 }
 
